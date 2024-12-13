bb850742a4712155ff1864dc341779fae5f188ab
Gaurang Nandaniya <gaurang1234522@gmail.com>
2024-12-13 21:44:26 +0530

    feat: pagination UI

--- project-list-saas-labs-assignment/src/App.jsx
+++ project-list-saas-labs-assignment/src/App.jsx
@@ -1,18 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { getData } from "./utils";
 import { Spinner, Table } from "./components";
 import classes from "./App.module.css";
-
-const COLUMNS = [
-  { key: "s.no", header: "S.No" },
-  { key: "percentage.funded", header: "Percentage funded" },
-  { key: "amt.pledged", header: "Amount pledged" },
-];
+import { COLUMNS, PAGE_SIZE } from "./constants";
 
 const App = () => {
   const [showSpinner, setShowSpinner] = useState(true);
   const [showError, setShowError] = useState(false);
   const [projectsData, setProjectsData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     //fetch data and store in state
@@ -30,14 +26,64 @@ const App = () => {
     );
   }, []);
 
+  const currentPageData = useMemo(() => {
+    const pageStartIndex = (currentPage - 1) * PAGE_SIZE;
+    const totalProjectData = projectsData?.length;
+
+    const pageData = [];
+    for (
+      let index = 0;
+      index < Math.min(PAGE_SIZE, totalProjectData - pageStartIndex);
+      index++
+    ) {
+      //Pushing page data to array
+      pageData.push(projectsData[pageStartIndex + index]);
+    }
+    return pageData;
+  }, [currentPage, projectsData]);
+
+  const totalPage = Math.ceil(projectsData?.length / PAGE_SIZE) || 0;
+
+  const PageNumberComp = () => {
+    const pages = [];
+    for (let index = 0; index < totalPage; index++) {
+      const isActive = index + 1 == currentPage;
+      pages.push(
+        <button
+          key={index}
+          className={`${classes.pageItem} ${
+            isActive ? classes.pageItemActive : ""
+          }`}
+          onClick={() => {
+            setCurrentPage(index + 1);
+          }}
+        >
+          {index + 1}
+        </button>
+      );
+    }
+
+    return <div className={classes.pageNumberContainer}>{pages} </div>;
+  };
+
   return (
     <div className={classes.container}>
       {showSpinner ? (
         <Spinner />
       ) : (
-        <Table columns={COLUMNS} data={projectsData} />
+        <>
+          <h1>Highly-rated Kickstarter Projects </h1>
+          <section className={classes.tableContainer}>
+            <Table columns={COLUMNS} data={currentPageData} />
+          </section>
+          {<PageNumberComp />}
+        </>
       )}
-      {showError && <h6>Opps... Something went wrong. Try again</h6>}
+      {showError && (
+        <h3 className={classes.errorText}>
+          Opps... Something went wrong. Try again
+        </h3>
+      )}
     </div>
   );
 };
---
+++ project-list-saas-labs-assignment/src/constants.js
@@ -0,0 +1,7 @@
+export const COLUMNS = [
+  { key: "s.no", header: "S.No" },
+  { key: "percentage.funded", header: "Percentage funded" },
+  { key: "amt.pledged", header: "Amount pledged" },
+];
+
+export const PAGE_SIZE = 5;
